51c6be5a42b26e6b2a86d394ad1616ff1565f762
Ariel Lopes De Souza <ariel.edit@gmail.com>
2020-11-06 18:56:20 -0400

    Testando Rest

---
+++ src/main/java/com/localize/LocalizaSeFacilApplication.java
@@ -0,0 +1,13 @@
+package com.localize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizaSeFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizaSeFacilApplication.class, args);
+	}
+
+}
